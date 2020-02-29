a97118629163c34c3917c88f3d04b00b4c2bb08f
fabi <fabian.kropfhamer@gmail.com>
2020-02-29 17:20:49 +0100

    added darkmode cross sites

--- public/darkmode.js
+++ public/darkmode.js
@@ -1,14 +1,19 @@
 function main() {
-    // global.localStorage.getItem("mySharedData")
-    console.log("darkmode");
+    const darkModeConfig = global.localStorage.getItem("darkmode") === "true";
+    darkmode(darkModeConfig);
     const darkmodeCheckBox = document.getElementById('darkmode');
+    darkmodeCheckBox.checked = darkModeConfig;
     darkmodeCheckBox.onclick = () => darkmode(darkmodeCheckBox.checked);
+    
 }
 
 function darkmode(isDarkMode) {
+    global.localStorage.setItem("darkmode", isDarkMode);
     const body = document.getElementsByTagName('body')[0]
     body.style.backgroundColor = isDarkMode ? 'black' : 'white';
     body.style.color = isDarkMode ? 'white' : 'black';
+
 }
+
 
 window.onload = main();
